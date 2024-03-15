db0dea635d4f8ee0f50f5229b0fd91675574791e
arovil7777 <arovil7777@naver.com>
2024-03-15 22:19:29 +0900

    정적 파일 경로 추가

--- nestjs-test/simple-chat/src/main.ts
+++ nestjs-test/simple-chat/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.static(join(__dirname, '..', 'client')));
   await app.listen(3000);
 }
 bootstrap();
