df3626af79417bbdde57952c65a36382bf650e38
arovil7777 <arovil7777@naver.com>
2024-03-27 00:01:08 +0900

    update chat.gateway.ts - 클라이언트 연결 종료 처리

--- nestjs-test/simple-chat/src/chat/chat.gateway.ts
+++ nestjs-test/simple-chat/src/chat/chat.gateway.ts
@@ -7,8 +7,8 @@ export class ChatGateway {
   server: Server;
 
   connectedClients: { [socketId: string]: boolean } = {};
-  clientNickName: { [socketId: string]: boolean } = {};
-  roomUsers: { [key: string]: boolean } = {};
+  clientNickName: { [socketId: string]: string } = {};
+  roomUsers: { [key: string]: string[] } = {};
 
   handleConnection(client: Socket): void {
     // 이미 연결되어 있는 클라이언트인지 확인합니다.
@@ -18,6 +18,34 @@ export class ChatGateway {
     }
 
     this.connectedClients[client.id] = true;
+  }
+
+  handleDisconnect(client: Socket): void {
+    delete this.connectedClients[client.id];
+
+    // 클라이언트 연결이 종료되면 해당 클라이언트가 속한 모든 방에서 유저를 제거합니다.
+    Object.keys(this.roomUsers).forEach((room) => {
+      const index = this.roomUsers[room]?.indexOf(this.clientNickName[client.id]);
+
+      if (index !== -1) {
+        this.roomUsers[room].splice(index, 1);
+        this.server.to(room)
+          .emit('userLeft', { userId: this.clientNickName[client.id], room });
+        this.server.to(room)
+          .emit('userList', { room, userList: this.roomUsers[room] });
+      }
+    });
+
+    // 모든 방의 유저 목록을 업데이트하여 emit합니다.
+    Object.keys(this.roomUsers).forEach((room) => {
+      this.server.to(room)
+        .emit('userList', { room, userList: this.roomUsers[room] });
+    });
+
+    // 연결된 클라이언트 목록을 업데이트하여 emit합니다.
+    this.server.emit('userList', {
+      room: null, userList: Object.keys(this.connectedClients)
+    });
   }
 
   @SubscribeMessage('message')
