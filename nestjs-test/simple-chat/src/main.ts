ab463a79a7f988a7d8ef5d7383063e7b6c6fb7c6
arovil7777 <arovil7777@naver.com>
2024-03-31 23:03:02 +0900

    update main.ts - 정적 파일 경로 수정

--- nestjs-test/simple-chat/src/main.ts
+++ nestjs-test/simple-chat/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import express from 'express';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
