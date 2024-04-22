7928ef5d921dc9a63cc1d71d9db21b164f8054a4
arovil7777 <arovil7777@naver.com>
2024-04-22 20:48:02 +0900

    update main.ts

--- nestjs-test/simple-chat/src/main.ts
+++ nestjs-test/simple-chat/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(express.static(join(__dirname, '..', 'client')));
+  app.useStaticAssets(join(__dirname, '..', 'client'));
   await app.listen(3000);
 }
 bootstrap();
